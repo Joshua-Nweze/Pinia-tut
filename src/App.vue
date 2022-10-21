96b7da5e7252d25894108b97fd5b8c8a7abeee1a
Joshua <joshuanweze270@gmail.com>
2022-10-21 07:30:21 +0100

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
 
-  <h1>Hello!</h1>
+  <h1>Hello! from {{ name }}</h1>
   <h3>{{ counter }}</h3>
-  <button @click="main.oneCount">Count</button>
+  <button @click="oneCount">Count</button>
+  <button @click="add(10)">Add 10</button>
+  <button @click="reset">Reset</button>
+  <button @click="randomNum">Random number</button>
 
 </template>
 
 <script>
 import { useCounterStore } from './store/useCounter';
-import { storeToRefs } from "pinia";
+import { storeToRefs, mapActions } from "pinia";
 
 export default {
   setup () {
     const main = useCounterStore();
+
     const { counter, name } = storeToRefs(main);
 
-    return { counter, name }
+    // Grabs an action from the store
+    const { oneCount } = mapActions(useCounterStore, ["oneCount"]);
+    //This works fine as the code above
+    // const { oneCount } = main;
+
+    function add(num) {
+      //Add changes to state
+      main.$patch(() => counter.value += num)
+      // This works fine as the code above
+      // main.$patch(state => state.counter += num)
+    }
+
+    function reset() {
+      //resets the state to its original state
+      main.$reset()
+    }
+
+    //watch for changes in the state
+    main.$subscribe((mutation, state) => {
+      console.log("mutaion", mutation);
+      console.log("state", state);
+    })
+
+    function randomNum() {
+      // Set state value to something else
+      main.$state = {
+        counter: Math.floor(Math.random() * 200) 
+      }
+    }
+
+
+    return { main, counter, name, oneCount, add, reset, randomNum };
   }
 }
 </script>
